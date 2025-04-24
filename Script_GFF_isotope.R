27f88f919a39a812fcd88bf1cfd8173a3403732b
DavidChnd <david.chassagnaud@gmail.com>
2025-04-24 15:29:12 +0200

    qmd et script au propre

--- Script_GFF_isotope.R
+++ Script_GFF_isotope.R
@@ -2,6 +2,7 @@
 
 library(readxl)
 library(ggplot2)
+library(dplyr)
 
 #### Carbone ####
 GFF_isotopie_C <- read_excel("~/Desktop/GFF_isotopie_C.xlsx", 
@@ -34,6 +35,32 @@ ggplot(GFF_isotopie_C, aes(x = years, y = d13C, color = Prov)) +
     axis.text.x = element_text(angle = 90, hjust = 1),
     plot.title = element_text(face = "bold", size = 14)
   )
+
+# Pour ploter Carbone avec la même chronologie que l'oxygène 
+
+#Créer une copie du jeu de données avec years en numérique
+GFF_isotopie_C$years_num <- as.numeric(as.character(GFF_isotopie_C$years))
+
+# Filtrage de la plage 2000–2009
+GFF_isotopie_C_filt <- GFF_isotopie_C %>%
+  filter(years_num >= 2000 & years_num <= 2009)
+
+ggplot(GFF_isotopie_C_filt, aes(x = years, y = d13C, color = Prov)) +
+  geom_point(alpha = 0.7, size = 2) +
+  stat_summary(fun = mean, geom = "line", aes(group = Prov), linetype = "solid", size = 0.8) +
+  facet_wrap(~site, labeller = labeller(site = function(x) paste("site :", x))) +
+  theme_minimal() +
+  labs(
+    title = "Chronologie de δ13C (2000–2009) par site et provenance",
+    x = "Année",
+    y = expression(delta^{13}*C~("\u2030")),
+    color = "Provenance"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1),
+    plot.title = element_text(face = "bold", size = 14)
+  )
+
 
 #### Oxygène ####
 
